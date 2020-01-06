4725cdc183debdebd53de12005de5d20de726a8f
yaconit <517837215@qq.com>
2020-01-06 09:59:12 +0800

    1

--- 第04节 JavaScript/新闻列表/public.js
+++ 第04节 JavaScript/新闻列表/public.js
@@ -41,8 +41,37 @@ function action(choose){
     }
 }
 
+// 关闭提示框
 function alert_ok(btn){
     btn.parentNode.parentNode.style.opacity=0
     let select = document.getElementsByName('actions')[0]
     select.options[0].selected = true
+}
+
+function show_item(tr){
+    let panels = document.getElementsByClassName('panels')[0]
+    panels.style.display = 'flex'
+
+    // let td_index = tr.querySelectorAll('td')[1]
+    // let td_title = tr.querySelectorAll('td')[2]
+    // let td_datetime = tr.querySelectorAll('td')[3]
+    // let td_author = tr.querySelectorAll('td')[4]
+    // let td_hits = tr.querySelectorAll('td')[5]
+    let td_index = tr.querySelector('td:nth-child(2)')
+    let td_title = tr.querySelector('td:nth-child(3)')
+    let td_datetime = tr.querySelector('td:nth-child(4)')
+    let td_author = tr.querySelector('td:nth-child(5)')
+    let td_hits = tr.querySelector('td:nth-child(6)')
+
+    let table = panels.querySelector('.panel table')
+    table.querySelector("tr:nth-child(1) td").innerText = td_index.innerText
+    table.querySelector("tr:nth-child(2) td").innerText = td_title.innerText
+    table.querySelector("tr:nth-child(3) td").innerText = td_datetime.innerText
+    table.querySelector("tr:nth-child(4) td").innerText = td_author.innerText
+    table.querySelector("tr:nth-child(5) td").innerText = td_hits.innerText
+}
+
+function close_all(){
+    let panels = document.getElementsByClassName('panels')[0]
+    panels.style.display = 'none'
 }
