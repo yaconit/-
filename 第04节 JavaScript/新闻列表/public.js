fefedc61dde4a57fa8ff1b2a9e82f3bf36229aaf
yaconit <517837215@qq.com>
2020-01-08 12:16:27 +0800

    1

--- 第04节 JavaScript/新闻列表/public.js
+++ 第04节 JavaScript/新闻列表/public.js
@@ -27,6 +27,11 @@ function del_item(btn) {
     btn.parentNode.parentNode.remove()
 }
 
+function show_alert(msg) {
+    document.getElementsByClassName('alert')[0].style.opacity = 1
+    document.getElementsByClassName('alert-body')[0].innerText = msg
+}
+
 // 多选操作
 function action(choose) {
     // 删除
@@ -37,8 +42,7 @@ function action(choose) {
                 checkboxes[i].parentNode.parentNode.remove()
             }
         }
-        document.getElementsByClassName('alert')[0].style.opacity = 1
-        document.getElementsByClassName('alert-body')[0].innerText = '删除成功'
+        show_alert('删除成功')
     }
     // 查看
     else if (choose == 2) {
@@ -63,6 +67,7 @@ function action(choose) {
 // 关闭提示框
 function alert_ok(btn) {
     btn.parentNode.parentNode.style.opacity = 0
+    btn.parentNode.parentNode.remove()
     let select = document.getElementsByName('actions')[0]
     select.options[0].selected = true
 }
@@ -134,7 +139,98 @@ function createElementFromString(str_dom) {
 }
 
 
+function add_item(tr) {
+    let title = tr.querySelector('td:nth-child(3) input').value
+    let datetime = tr.querySelector('td:nth-child(4) input').value
+    let author = tr.querySelector('td:nth-child(5) select').value
+    let hits = tr.querySelector('td:nth-child(6) input').value
+
+    if (title.length == 0) {
+        show_alert('请填写标题')
+        return
+    }
+
+    let index = document.querySelector('tbody tr:last-child td:nth-child(2)').innerText
+    index = parseInt(index) + 1
+    console.log(index)
+
+    let new_tr = document.createElement('tr')
+    let td1 = document.createElement('td')
+    let td2 = document.createElement('td')
+    let td3 = document.createElement('td')
+    let td4 = document.createElement('td')
+    let td5 = document.createElement('td')
+    let td6 = document.createElement('td')
+    let td7 = document.createElement('td')
+
+    td1.innerHTML = '<input type="checkbox" class="item_checkbox" onchange="item_checked()">'
+    td2.innerText = index
+    td3.innerText = title
+    td4.innerText = datetime
+    td5.innerText = author
+    td6.innerText = hits
+    td7.innerHTML = '<button onclick="show_item(this.parentNode.parentNode)">查看</button><button>修改</button><button onclick="del_item(this)">删除</button>'
+
+    new_tr.appendChild(td1)
+    new_tr.appendChild(td2)
+    new_tr.appendChild(td3)
+    new_tr.appendChild(td4)
+    new_tr.appendChild(td5)
+    new_tr.appendChild(td6)
+    new_tr.appendChild(td7)
+
+    document.querySelector('tbody').appendChild(new_tr)
+    show_alert('添加成功')
+    clear_input()
+}
+
+function clear_input() {
+    document.querySelector('tfoot tr td:nth-child(3) input').value = ''
+    document.querySelector('tfoot tr td:nth-child(4) input').value = ''
+    document.querySelector('tfoot tr td:nth-child(5) select').options[0].selected = true
+    document.querySelector('tfoot tr td:nth-child(6) input').value = ''
+}
 
 
+function edit_text(td) {
+    // 防止生成多个 input
+    if(td.querySelectorAll('input').length > 0){
+        return
+    }
 
+    let span = td.querySelector('span')
+    span.style.display = 'none'
 
+    // 获取 td 原来的文本
+    let text = span.innerText
+
+    // 创建 input 对象
+    let input = document.createElement('input')
+    // 设置 input 的属性
+    input.setAttribute('type', 'text')
+    input.value = text
+
+    // 将 input 追加到 td 中
+    td.appendChild(input)
+
+    input.focus() // 给 input 设置焦点
+    input.select() // 选中 input 中的文本
+
+    // 给 input 设置 keydown 事件
+    // input.addEventListener('keydown',function(event){
+    //     if(event.key == 'Enter'){
+    //         span.innerText = input.value
+    //         input.remove()
+    //         span.style.display = 'inline'
+    //     }
+    // })
+
+    input.onkeydown = function (event) {
+        if (event.key == 'Enter') {
+            span.innerText = input.value
+            input.remove()
+            span.style.display = 'inline'
+        }
+    }
+
+}
