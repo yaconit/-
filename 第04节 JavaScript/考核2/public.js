0ea4bd97c205eeb6e1105310d83aaba26431a860
yaconit <517837215@qq.com>
2020-01-08 10:40:47 +0800

    1

--- 第04节 JavaScript/考核2/public.js
+++ 第04节 JavaScript/考核2/public.js
@@ -1,17 +1,26 @@
 function add_img(){
-    console.log(1)
+    // 选择 ul 元素
     let ul = document.getElementsByTagName('ul')[0]
-    let li = document.createElement('li')
     
+    // 创建 li 元素（DOM对象）
+    let li = document.createElement('li')
+    // 创建 li 内部元素（DOM字符串）
     let img = '<img src="./images/'+(Math.floor(Math.random()*6)+1)+'.jpg"/>'
     let h3 = '<h3>图片</h3>'
     let i = '<i class="iconfont icon-lajixiang" onclick="remove(this.parentNode)"></i>'
-    
-    li.innerHTML = img + '\n' + h3 + '\n' + i
-    li.style.opacity = 0
-    ul.appendChild(li)
-    let opacity = 0
+    let button = '<button>查看详情</button>'
+   
+    // 将 li 内部元素 插入到 li 元素中
+    li.innerHTML = img + '\n' + h3 + '\n' + i + '\n' + button
 
+    // 设置 li 的透明度（隐藏）
+    li.style.opacity = 0
+
+    // 将 li 添加到 ul 中
+    ul.appendChild(li)
+
+    // 设置 li 的透明度（显示）
+    let opacity = 0
     let st = setInterval(function(){
         opacity += 0.01
         li.style.opacity = opacity
